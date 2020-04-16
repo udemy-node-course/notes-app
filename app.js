3a9c2f9f3e87666d9176a63cb09607a100f050e2
sgigli <simon.luciano@gigli.net>
2020-04-16 14:37:11 -0400

    Building out notes functions to handle our notes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const chalk = require('chalk')
 const yargs = require('yargs')
-const getNotes = require('./notes.js')
+const notes = require('./notes.js')
 
 // Custome yargs version 
 yargs.version('1.1.0')
@@ -22,8 +22,7 @@ yargs.command({
         }
     },
     handler: function (argv) {
-        console.log('Title: ' + argv.title,
-         '\nBody: ' + argv.body)
+        notes.addNote(argv.title, argv.body)
     }
 })
 
